028b6cdb8e8e104a7ae12b8756809a00c3d99cf6
Adefranky <155418329+Adefranky@users.noreply.github.com>
2024-09-24 21:06:19 +0300

    default parameters

--- app.js
+++ app.js
@@ -97,3 +97,28 @@ function user(name, age, work){
  const johnstone = user('stone', 20, ' developer')
  console.log(frank.intro());
  console.log(johnstone.intro());
+
+//  Default function parameters , introduced in ECMASCRIPT 6 (ES6), allow you to assign default values to function parameters. When a function is called , and an argument is not provided for a parameter, the dafault value will be used insted. the feature makes it easier to handle cases where arguments might be missing or undefined.
+function countTo5(count = false){
+if(count === true){
+    for (let i = 1; i <= 5; i++){
+        console.log(`count: ${i}`);
+    }
+}
+}
+countTo5(true);
+
+function ratings(rate = 0){
+    if (rate ===5){
+        console.log(`High ratings :)`);
+    } else if (rate === 0 ){
+        console.log(`Low Ratings :(`);
+    }
+}
+ratings(5);
+
+// challenge
+function multiply(a,b = 1){
+    return a * b
+}
+console.log(multiply(5))
