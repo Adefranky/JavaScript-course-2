c00d8cae3116c6eecb3921d934067fb639ff5452
Adefranky <155418329+Adefranky@users.noreply.github.com>
2024-10-01 16:49:14 +0300

    symol

--- app.js
+++ app.js
@@ -711,3 +711,35 @@ console.log(leters);
 for (let value of leters) {
     console.log(value);
 }
+
+// Symbol is a unique and immutable primitive data type introduced in ECMASript 6 (ES6). A symbol is often used as an identifier  for object properties to avoid potential naming conflicts.
+
+// Unlike strings or numbers, symbols are guaranteed to be unique. When you create a symbol , it is unique and cannot be recreated or changed. Tis uniqueness ensures tat symbols will not collide with other property names , even if they have the same string representation.
+
+const mySymbol = Symbol("My custom symbol");
+console.log(mySymbol);
+
+// Comparing symbols
+const mySymbol1 = Symbol("name");
+const mySymbol2 = Symbol("name");
+console.log(mySymbol1 === mySymbol2);
+
+const obj = {};
+obj[mySymbol1] = "value 1";
+obj[mySymbol2] = "value 2";
+
+console.log(obj);
+
+let sym = Symbol("foo");
+console.log(typeof sym);
+
+let obje = {
+    firstName: "Frank",
+    lastNmae: "Webdev"
+};
+obj[Symbol("foo")] = "some value for foo";
+console.log(obje);
+
+for (let i in obje){
+    console.log(i);
+}
